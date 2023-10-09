c683bb4d3886947992b0cf2dbf7209076d2c1f3d
Rodrigo Fontes <rodrigofontes377@gmail.com>
2023-10-09 11:11:25 -0300

    node

--- App.ts
+++ App.ts
@@ -11,13 +11,13 @@ export class App {
     console.log(
       "Bem vindo ao Banco Maut. Insira seus dados para criar uma conta:"
     );
-    rl.question("Nome: ", (nome) => {});
+    rl.question("Nome: ", (nome: any) => {});
   }
 
   exibirMenu(cliente: Cliente, conta: Conta) {
     rl.question(
       "\n//-- Banco Maut --//\nEscolha uma operação:\n1 ** Minhas informações\n2 ** Saldo\n3 ** Depositar\n4  ** Sacar\n5 ** Transferir\n6 ** Extrato Bancário\n7 ** Mudança de Dados\n8 - Sair\nOpção: ",
-      (opcaoStr) => {
+      (opcaoStr: string) => {
         const opcao = parseInt(opcaoStr);
 
         switch (opcao) {
